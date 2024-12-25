1139cb9c0c1da496cd87905ed933571e55a0f8da
Angel Almanza <almanza.angel245@gmail.com>
2024-12-25 15:57:34 -0700

    Base project

---
+++ constants/Enviroment.ts
@@ -0,0 +1,15 @@
+const firebaseApiKey = process.env.EXPO_PUBLIC_API_KEY;
+const firebaseAuthDomain = process.env.EXPO_PUBLIC_AUTH_DOMAIN;
+const firebaseProjectId = process.env.EXPO_PUBLIC_PROJECT_ID;
+const firebaseStorageBucket = process.env.EXPO_PUBLIC_STORAGE_BUCKET;
+const firebaseMessagingSenderId = process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID;
+const firebaseAppId = process.env.EXPO_PUBLIC_APP_ID;
+
+export {
+  firebaseApiKey,
+  firebaseAuthDomain,
+  firebaseProjectId,
+  firebaseStorageBucket,
+  firebaseMessagingSenderId,
+  firebaseAppId,
+};
---
+++ lib/firebaseConfig.ts
@@ -0,0 +1,24 @@
+import {
+  firebaseApiKey,
+  firebaseAppId,
+  firebaseAuthDomain,
+  firebaseMessagingSenderId,
+  firebaseProjectId,
+  firebaseStorageBucket,
+} from "@/constants/Enviroment";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: firebaseApiKey,
+  authDomain: firebaseAuthDomain,
+  projectId: firebaseProjectId,
+  storageBucket: firebaseStorageBucket,
+  messagingSenderId: firebaseMessagingSenderId,
+  appId: firebaseAppId,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { app, db };
